1889f99bcb40e1096aac7c3c13ed02de4748f7a7
Myers <mmbonda@lcs.on.ca>
2024-01-15 12:06:08 -0500

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Myers Elliott Mbonda on 2024-01-15.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
