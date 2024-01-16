ae9ea9684785a70ed68b7e8a77d7d6bc4c7008f0
Myers <mmbonda@lcs.on.ca>
2024-01-15 19:59:58 -0500

    Finished entire project

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,17 +8,231 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var base: Int = 1
+    @State var base2: Int = 1
+    
+    var addition: Int {
+        return base + base2
+    }
+    var substraction: Int {
+        return base - base2
+    }
+    var multiplication: Int {
+        return base * base2
+    }
+    var division: Int {
+        return base / base2
+    }
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        TabView {
+            VStack {
+                Spacer()
+                
+                HStack(alignment: .top){
+                    Spacer()
+                    Text("\(base)")
+                        .font(.system(size: 66))
+                    
+                    
+                    
+                    
+                    
+                }
+                
+                VStack {
+                    Stepper(value: $base, label: {
+                        Text("Select first number")
+                    })
+                    HStack {
+                        Text("+").font(.system(size: 46))
+                        Spacer()
+                        Text("\(base2)")
+                            .font(.system(size: 66))
+                    }
+                    Stepper(value: $base2, label: {
+                        Text("Select second number")
+                    })
+                    
+                    HStack {
+                        Text("=").font(.system(size: 66))
+                        Spacer()
+                        Text("\(addition)").font(.system(size: 66))
+                        
+                        
+                    }
+                    
+                    
+                }
+                
+                Spacer()
+                
+                
+            }
+            .padding()
+            .tabItem {
+                Image(systemName: "plus.square.fill")
+                Text("Addition")
+                
+                
+                
+            }
+            VStack {
+                Spacer()
+                
+                HStack(alignment: .top){
+                    Spacer()
+                    Text("\(base)")
+                        .font(.system(size: 66))
+                    
+                    
+                    
+                    
+                    
+                }
+                
+                VStack {
+                    Stepper(value: $base, label: {
+                        Text("Select first number")
+                    })
+                    HStack {
+                        Text("+").font(.system(size: 46))
+                        Spacer()
+                        Text("\(base2)")
+                            .font(.system(size: 66))
+                    }
+                    Stepper(value: $base2, label: {
+                        Text("Select second number")
+                    })
+                    
+                    HStack {
+                        Text("=").font(.system(size: 66))
+                        Spacer()
+                        Text("\(substraction)").font(.system(size: 66))
+                        
+                        
+                    }
+                    
+                    
+                }
+                
+                Spacer()
+                
+                
+            }
+            .padding()
+            .tabItem {
+                Image(systemName: "minus.square.fill")
+                Text("Substraction")
+            }
+            VStack {
+                Spacer()
+                
+                HStack(alignment: .top){
+                    Spacer()
+                    Text("\(base)")
+                        .font(.system(size: 66))
+                    
+                    
+                    
+                    
+                    
+                }
+                
+                VStack {
+                    Stepper(value: $base, label: {
+                        Text("Select first number")
+                    })
+                    HStack {
+                        Text("x").font(.system(size: 46))
+                        Spacer()
+                        Text("\(base2)")
+                            .font(.system(size: 66))
+                    }
+                    Stepper(value: $base2, label: {
+                        Text("Select second number")
+                    })
+                    
+                    HStack {
+                        Text("=").font(.system(size: 66))
+                        Spacer()
+                        Text("\(multiplication)").font(.system(size: 66))
+                        
+                        
+                    }
+                    
+                    
+                }
+                
+                Spacer()
+                
+                
+            }
+            .padding()
+            .tabItem {
+                Image(systemName: "plus.square.fill")
+                Text("Multiplication")
+                
+                
+                
+            }
+            VStack {
+                Spacer()
+                
+                HStack(alignment: .top){
+                    Spacer()
+                    Text("\(base)")
+                        .font(.system(size: 66))
+                    
+                    
+                    
+                    
+                    
+                }
+                
+                VStack {
+                    Stepper(value: $base, label: {
+                        Text("Select first number")
+                    })
+                    HStack {
+                        Text("÷").font(.system(size: 46))
+                        Spacer()
+                        Text("\(base2)")
+                            .font(.system(size: 66))
+                    }
+                    Stepper(value: $base2, label: {
+                        Text("Select second number")
+                    })
+                    
+                    HStack {
+                        Text("=").font(.system(size: 66))
+                        Spacer()
+                        Text("\(division)").font(.system(size: 66))
+                        
+                        
+                    }
+                    
+                    
+                }
+                
+                Spacer()
+                
+                
+            }
+            .padding()
+            .tabItem {
+                Image(systemName: "divide.square.fill")
+                Text("Division")
+                
+                
+                
+            }
+            
         }
-        .padding()
     }
 }
-
 #Preview {
     ContentView()
-}
+    }
+
